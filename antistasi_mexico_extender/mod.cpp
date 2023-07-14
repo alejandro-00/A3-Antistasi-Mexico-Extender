5cf3717f367fe0a7220fd309fa49f140da51163b
alejandro00 <g_alejandro_dev@outlook.com>
2023-07-13 21:32:53 -0600

    mod.cpp modificado

--- antistasi_mexico_extender/mod.cpp
+++ antistasi_mexico_extender/mod.cpp
@@ -2,4 +2,5 @@ name = "A3 Antistasi Mexico Extender";
 author = "alejandro00";
 tooltip = "Antistasi Mexico Extender";
 actionName = "GitHub";
-action = "https://github.com/HakonRydland/A3AExtender";
+action = "https://github.com/alejandro-00/A3-Antistasi-Mexico-Extender";
+ 
